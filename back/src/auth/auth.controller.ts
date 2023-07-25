da88cd675f8177aaad2c9b06d7951a6a0b1176f6
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-07-25 18:42:26 -0400

    Implement the signup post feature

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.authService.createUser(createUserDto);
+  }
+}
--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -2,13 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
-  ) { }
-  
-  
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const { username, password } = createUserDto;
+    const user = this.usersRepository.create({
+      username,
+      password,
+    });
+    await this.usersRepository.save(user);
+    return user;
+  }
 }
