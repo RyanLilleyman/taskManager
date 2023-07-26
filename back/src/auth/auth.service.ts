7f0e33a29944e355d4b3c153b47e6bf34c77f5a5
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-07-26 03:45:14 -0400

    Add hashing and salting to login credentials

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  ConflictException,
+} from '@nestjs/common';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,16 +18,22 @@ export class AuthService {
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { username, password } = createUserDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.usersRepository.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
     try {
       await this.usersRepository.save(user);
     } catch (error) {
-      console.log(error.code);
-      if (error.code === '23503') {
+      console.log(error);
+      if (
+        error.code === '23505' ||
+        (error.originalError && error.originalError.code === '23505')
+      ) {
         throw new ConflictException('Username already exists');
       } else {
         throw new InternalServerErrorException();
