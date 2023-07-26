211b64f5455e58350452e74461dd983eab56b4fc
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-07-26 17:14:44 -0400

    remove front

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -18,7 +18,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
           type: 'postgres',
           host: configService.get('DB_HOST'),
           port: configService.get('DB_PORT'),
-          username: configService.get('DB_USER'),
+          username: configService.get('DB_USERNAME'),
           password: configService.get('DB_PASSWORD'),
           database: configService.get('DB_NAME'),
           entities: [],
